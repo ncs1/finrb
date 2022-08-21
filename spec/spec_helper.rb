443e01e8151f13b612a79786167981e61ca68f3a
Nadir Cohen <nadircs11@gmail.com>
2022-08-21 04:18:13 +0300

    profile examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,7 @@ require_relative('../lib/finrb/utils')
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   include(Finrb)
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -92,11 +93,13 @@ RSpec.configure do |config|
   #     config.default_formatter = "doc"
   #   end
   #
-  #   # Print the 10 slowest examples and example groups at the
-  #   # end of the spec run, to help surface which specs are running
-  #   # particularly slow.
-  #   config.profile_examples = 10
-  #
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+
   #   # Run specs in random order to surface order dependencies. If you find an
   #   # order dependency and want to debug it, you can fix the order by providing
   #   # the seed, which is printed after each run.

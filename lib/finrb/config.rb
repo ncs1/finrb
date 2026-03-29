eb58bddd8e88a096e198564701089e7d30251308
Stuart Chinery <stuart.chinery@boxt.co.uk>
2026-03-29 18:54:00 +0100

    Rubocop lint fixes

--- lib/finrb/config.rb
+++ lib/finrb/config.rb
@@ -2,12 +2,13 @@
 
 module Finrb
   Config = Struct.new(:eps, :guess, :business_days, :periodic_compound)
+  private_constant :Config
 
   def self.config
     @config ||= Config.new('1.0e-16', 1.0, false, false)
   end
 
   def self.configure
-    yield config
+    yield(config)
   end
 end
